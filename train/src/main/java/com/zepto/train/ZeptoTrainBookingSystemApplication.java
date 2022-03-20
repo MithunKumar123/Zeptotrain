422de901a581fd57a4e4c3e06e256d40fa28c0c0
Mithunkumar123 <mithun.selvaraj@gmail.com>
2022-03-21 00:47:30 +0530

    initial push with empty code

---
+++ train/src/main/java/com/zepto/train/ZeptoTrainBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.zepto.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeptoTrainBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeptoTrainBookingSystemApplication.class, args);
+	}
+
+}
---
+++ train/src/test/java/com/zepto/train/ZeptoTrainBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zepto.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeptoTrainBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
